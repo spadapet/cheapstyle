6a342aafb8a2935ddc6587e4f30aee6f3b522958
Peter Spada <spadapet@hotmail.com>
2015-01-09 13:24:43 -0800

    Start a WPF app

---
+++ CheapStyle/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+
+namespace CheapStyle
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnChooseStyle(object sender, RoutedEventArgs args)
+        {
+
+        }
+
+        private void OnChooseDest(object sender, RoutedEventArgs args)
+        {
+
+        }
+
+        private void OnOk(object sender, RoutedEventArgs args)
+        {
+
+        }
+
+        private void OnCancel(object sender, RoutedEventArgs args)
+        {
+
+        }
+    }
+}
--- pch.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-// Windows
-#include <Windows.h>
-
-// CRT
-#include <string>
