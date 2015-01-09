6fed593274b40b3c39cd5390659918e223139f9a
Peter Spada <spadapet@hotmail.com>
2015-01-09 12:55:22 -0800

    Nothing really

--- CheapStyle.cpp
+++ CheapStyle.cpp
@@ -2,5 +2,21 @@
 
 int wmain(int argc, TCHAR* argv[])
 {
+	std::wstring styleFile;
+	std::wstring outDir;
+
+	if (argc >= 2)
+	{
+		styleFile = argv[1];
+
+		if (argc >= 3)
+		{
+			outDir = argv[2];
+		}
+	}
+	else
+	{
+	}
+
 	return 0;
 }
--- pch.h
+++ pch.h
@@ -1,3 +1,7 @@
 #pragma once
 
+// Windows
 #include <Windows.h>
+
+// CRT
+#include <string>
