f8e1a839a18b140ddc1d750e9f80b6e27d6dbedf
Peter Spada <spadapet@hotmail.com>
2015-01-15 12:47:35 -0800

    Read data positions

--- CheapStyle/Style.cs
+++ CheapStyle/Style.cs
@@ -3,6 +3,21 @@ namespace CheapStyle
 {
     internal class Style
     {
+        private uint _posHeader;
+        private uint _posStyle;
+        private uint _posAuthor;
+        private uint _posStandards;
+        private uint _posSketches;
+        private uint _posErasers;
+        private uint _posLemmings;
+        private uint _posObjects;
+        private uint _posGraphics;
+        private uint _posFooter;
+        private uint _posMusic;
+        private uint _posSounds;
+        private uint _posImages;
+        private uint _posFiles;
+
         private Style()
         {
         }
@@ -32,6 +47,24 @@ namespace CheapStyle
             }
 
             MemoryStream stream = new MemoryStream(bytes);
+
+            _posFooter = stream.LoadUint();
+            stream.Position = _posFooter;
+
+            _posHeader = stream.LoadUint();
+            _posStyle = stream.LoadUint();
+            _posAuthor = stream.LoadUint();
+            _posStandards = stream.LoadUint();
+            _posSketches = stream.LoadUint();
+            _posErasers = stream.LoadUint();
+            _posLemmings = stream.LoadUint();
+            _posObjects = stream.LoadUint();
+            _posGraphics = stream.LoadUint();
+            _posMusic = stream.LoadUint();
+            _posSounds = stream.LoadUint();
+            _posImages = stream.LoadUint();
+            _posFiles = stream.LoadUint();
+
             return true;
         }
     }
